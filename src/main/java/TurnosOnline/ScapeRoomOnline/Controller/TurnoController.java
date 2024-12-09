4e78781e7f372d747508b322c398877397b8ed45
LautaroFavoni <lautarocarp4@hotmail.com>
2024-12-09 18:20:35 -0300

    asociando pagos

--- src/main/java/TurnosOnline/ScapeRoomOnline/Controller/TurnoController.java
+++ src/main/java/TurnosOnline/ScapeRoomOnline/Controller/TurnoController.java
@@ -22,6 +22,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,11 +47,16 @@ public class TurnoController {
             return new ResponseEntity<>("El importe pagado debe ser mayor que cero", HttpStatus.BAD_REQUEST);
         }
 
+
         Optional<Sala> sala = salaRepository.findById(turnoDTO.getSalaId());
-        if (sala.isPresent()) {
-            // Verificar si ya existe un turno en la misma sala a la misma hora
-            Optional<Turno> turnoExistente = turnoRepository.findBySalaIdAndDiaYHora(turnoDTO.getSalaId(), turnoDTO.getDiaYHora());
-            if (turnoExistente.isPresent() && turnoExistente.get().getPago().equalsIgnoreCase("true")) {
+        if (sala.isEmpty()) {
+            return new ResponseEntity<>("Sala no encontrada", HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            // Buscar turnos pagados en la misma sala y horario
+            List<Turno> turnos = turnoRepository.findPaidTurnosBySalaIdAndDiaYHora(turnoDTO.getSalaId(), turnoDTO.getDiaYHora());
+            if (!turnos.isEmpty()) {
                 return new ResponseEntity<>("Ya existe un turno reservado y pagado para esta sala a la misma hora", HttpStatus.CONFLICT);
             }
 
@@ -78,10 +86,17 @@ public class TurnoController {
                     .unitPrice(turnoDTO.getimporteTotal()) // Asegúrate de que sea BigDecimal
                     .build();
 
+            // Calcular la fecha y hora de expiración (15 minutos a partir de ahora)
+            LocalDateTime expirationDate = LocalDateTime.now(ZoneOffset.UTC).plusMinutes(15);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            String expirationDateTo = expirationDate.format(formatter);
+
+
             // Crear la preferencia
             PreferenceRequest preferenceRequest = PreferenceRequest.builder()
                     .items(List.of(itemRequest))
                     .externalReference(String.valueOf(savedTurno.getId()))
+                    .expirationDateTo(OffsetDateTime.parse(expirationDateTo)) // Establecer la fecha de expiración
                     .build();
 
             PreferenceClient preferenceClient = new PreferenceClient();
@@ -95,10 +110,54 @@ public class TurnoController {
 
                 // Enviar correo con el enlace de pago
                 String subject = "Confirmación de Turno - Pago Pendiente";
-                String body = "Tu turno ha sido reservado exitosamente para la sala: " + sala.get().getNombre() +
-                        " a las " + turnoDTO.getDiaYHora() + ". Para completar el pago, haz clic en el siguiente enlace: " +
-                        paymentLink;
+                String body = """
+                        <html>
+                            <body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+                                <div style="max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px;">
+                                    <h1 style="text-align: center; color: #4CAF50;">¡Tu reserva ha sido realizada con éxito!</h1>
+                                    <p>Hola <strong>%s</strong>,</p>
+                                    <p>Gracias por reservar con nosotros. Estamos emocionados de recibirte y hacer que vivas una experiencia inolvidable. Aquí están los detalles de tu reserva:</p>
+                                    <ul style="list-style-type: none; padding: 0;">
+                                        <li><strong>Fecha:</strong> %s</li>
+                                        <li><strong>Hora:</strong> %s</li>
+                                        <li><strong>Sala:</strong> %s</li>
+                                        <li><strong>Cantidad de jugadores:</strong> %d</li>
+                                        <li><strong>Total a abonar:</strong> $%s</li>
+                                    </ul>
+                                    <h3 style="color: #F57C00;">Estado de tu reserva</h3>
+                                    <p>Tu turno ha sido reservado y está actualmente <strong>pendiente de pago</strong>. Para confirmar tu reserva, por favor realiza el pago a través del siguiente enlace:</p>
+                                    <p style="text-align: center;">
+                                        <a href="%s" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Haz clic aquí para pagar</a>
+                                    </p>
+                                    <p>Una vez procesado el pago, recibirás un correo confirmando tu turno.</p>
+                                    <h3 style="color: #2196F3;">Información adicional</h3>
+                                    <ul style="list-style-type: none; padding: 0;">
+                                        <li>Por favor, llegar <strong>15 minutos antes</strong> de la hora programada para recibir las instrucciones.</li>
+                                        <li>Si necesitas reprogramar tu reserva, contáctanos con al menos <strong>24 horas de anticipación</strong>.</li>
+                                    </ul>
+                                    <h3 style="color: #FF5722;">Ubicación y contacto</h3>
+                                    <ul style="list-style-type: none; padding: 0;">
+                                        <li><strong>Dirección:</strong> Buenos Aires 1415, Rosario, Santa Fe</li>
+                                        <li><strong>Teléfono:</strong> +54 9 3417 03-7222</li>
+                                        <li><strong>Email:</strong> contacto@lockandkey.com.ar</li>
+                                    </ul>
+                                    <p style="text-align: center; font-weight: bold;">¡Te esperamos para vivir esta increíble aventura!</p>
+                                    <p style="text-align: center;">El equipo de <strong>Lock & Key</strong></p>
+                                </div>
+                            </body>
+                        </html>
+                        """.formatted(
+                        turnoDTO.getNombre(),  // Nombre del cliente
+                        turnoDTO.getDiaYHora().toLocalDate(),  // Fecha
+                        turnoDTO.getDiaYHora().toLocalTime(),  // Hora
+                        sala.get().getNombre(),  // Nombre de la sala
+                        turnoDTO.getJugadores(),  // Cantidad de jugadores
+                        turnoDTO.getimporteTotal(),  // Total a abonar
+                        paymentLink  // Enlace de pago
+                );
+
                 emailService.sendEmail(turnoDTO.getMail(), subject, body);
+
 
                 // Crear el objeto de respuesta con el turno y el enlace de pago
                 TurnoConLinkDePago response = new TurnoConLinkDePago(savedTurno, paymentLink);
@@ -110,10 +169,12 @@ public class TurnoController {
                 e.printStackTrace();
                 return new ResponseEntity<>("Error al procesar el pago con Mercado Pago: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
             }
-        } else {
-            return ResponseEntity.notFound().build(); // Sala no encontrada
+        }catch (Exception e) {
+            return new ResponseEntity<>("Error al procesar la solicitud: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
         }
     }
+
 
 
 
--- src/main/java/TurnosOnline/ScapeRoomOnline/Persistance/repository/TurnoRepository.java
+++ src/main/java/TurnosOnline/ScapeRoomOnline/Persistance/repository/TurnoRepository.java
@@ -2,6 +2,8 @@ package TurnosOnline.ScapeRoomOnline.Persistance.repository;
 
 import TurnosOnline.ScapeRoomOnline.Persistance.entities.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,4 +16,8 @@ public interface TurnoRepository extends JpaRepository <Turno, Long> {
 
 
     Optional<Turno> findByPreferenceId(String preferenceId);
+
+    @Query("SELECT t FROM Turno t WHERE t.sala.id = :salaId AND t.diaYHora = :diaYHora AND t.pago = 'true'")
+    List<Turno> findPaidTurnosBySalaIdAndDiaYHora(@Param("salaId") Long salaId, @Param("diaYHora") LocalDateTime diaYHora);
+
 }
